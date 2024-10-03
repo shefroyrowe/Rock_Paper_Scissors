619130afadf70c72c8c66d60e960fbb61b9b272f
_vision <shefroy.rowe@gmail.com>
2024-10-03 16:15:27 -0400

    add and complete playGame function/logic, refactor code, add final victory message

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
-let playerScore = 0;
-let computerScore = 0;
+//global variables
+let rounds = 0;
 
-//get computer guess
-const getComputerChoice = () => {
+//play game logic
+const playGame = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    //get computer guess
+const computerSelection = () => {
     let choice = '';
     const getRndom = Math.floor(Math.random() * 3);
     if (getRndom === 0) {
@@ -13,44 +18,66 @@ const getComputerChoice = () => {
         choice = "scissors";
     }
     return choice;
-}
+}//end get computer guess
 
-//get player guess
-const getHumanChoice = () => {
-    let playerChoice = prompt("Enter 'Rock, Paper, or Scissors' to play: ",).toLocaleLowerCase();
-    return playerChoice;
-}
+    //play one round logic
+    const playRound = (humanChoice, computerChoice) => {
 
-//play game function
-const playRound = (humanChoice, computerChoice) => {
-    if (humanChoice === computerChoice) {
-        let draw = `Player: ${humanChoice.toUpperCase()} Computer: ${computerChoice.toUpperCase()}. It's a draw!`;
-        console.log(draw);
-        return;
-    } else if (humanChoice === "rock" &&
-         computerChoice === "paper" ||
-         humanChoice === "scissors" &&
-         computerChoice === "rock" ||
-         humanChoice === "paper" &&
-         computerChoice === "scissors") {
-            let computerWin = `Player: ${humanChoice.toUpperCase()} Computer: ${computerChoice.toUpperCase()}. Compter wins!`;
+        if (humanChoice === computerChoice) {
+            let draw = `Player: ${humanChoice.toUpperCase()} Computer: ${computerChoice.toUpperCase()}. It's a draw!`;
+            console.log(draw);
+
+            console.log("Player: ", playerScore);
+            console.log("Pomputer: ", computerScore);
+            return;
+        } else if (humanChoice === "rock" &&
+            computerChoice === "paper" ||
+            humanChoice === "scissors" &&
+            computerChoice === "rock" ||
+            humanChoice === "paper" &&
+            computerChoice === "scissors") {
+            let computerWin = `Player: ${humanChoice.toUpperCase()} Computer: ${computerChoice.toUpperCase()}. Computer wins!`;
             computerScore += 1;
             console.log(computerWin);
+
+            console.log("Player: ", playerScore);
+            console.log("Computer: ", computerScore);
             return;
-    } else if (computerChoice === "rock" &&
-        humanChoice === "paper" ||
-        computerChoice === "scissors" &&
-        humanChoice === "rock" ||
-        computerChoice === "paper" &&
-        humanChoice === "scissors") {
-           let playerWin = `Player: ${humanChoice.toUpperCase()} Computer: ${computerChoice.toUpperCase()}. Player wins!`;
-           playerScore += 1;
-           console.log(playerWin);
-           return;
-   }
-}
+        } else if (computerChoice === "rock" &&
+            humanChoice === "paper" ||
+            computerChoice === "scissors" &&
+            humanChoice === "rock" ||
+            computerChoice === "paper" &&
+            humanChoice === "scissors") {
+            let playerWin = `Player: ${humanChoice.toUpperCase()} Computer: ${computerChoice.toUpperCase()}. Player wins!`;
+            playerScore += 1;
+            console.log(playerWin);
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+            console.log("Player: ", playerScore);
+            console.log("Computer: ", computerScore);
+            return;
+        }
+    }//end playRound function
 
-playRound(humanSelection, computerSelection);
+    //call playRound in loop to 5 rounds
+    do {
+        //prompt user while rounds not = 5 (get player guess)
+        const humanSelection = prompt("Enter 'Rock, Paper, or Scissors' to play: ",).toLocaleLowerCase();
+        //call playRound here
+        playRound(humanSelection, computerSelection());
+        //increment rounds by 1
+        rounds++;
+    } while (rounds < 5);//end do while loop
+
+
+    //final win logic
+    if (rounds === 5 && playerScore > computerScore) {
+        console.log("playerScore: " + playerScore, " Player wins! Flawless Victory!");
+    } else if (rounds === 5 && computerScore > playerScore) {
+        console.log("computerScore: " + computerScore, " Computer wins! Flawless Victory!");
+    }
+
+}//end playGame function
+
+//call playGame
+playGame();
